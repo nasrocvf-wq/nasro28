45a44adbaf7f795b5641e52aa174b1bec6abf73f
nasrocvf-wq <nasrocvf@gmail.com>
2026-08-28 07:32:43 +0100

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,17 +1,9 @@
-import java.util.Properties
-
 pluginManagement {
     val flutterSdkPath = run {
-        val properties = Properties()
-        file("local.properties").inputStream().use {
-            properties.load(it)
-        }
-
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
         val flutterSdkPath = properties.getProperty("flutter.sdk")
-        require(flutterSdkPath != null) {
-            "flutter.sdk not set in local.properties"
-        }
-
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
         flutterSdkPath
     }
 
@@ -26,14 +18,16 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+
     id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(
-        org.gradle.api.initialization.resolve.RepositoriesMode.PREFER_SETTINGS
-    )
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
 
     repositories {
         google()
